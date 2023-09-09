29f7c5b4d50b3a751cfbb6139e3ed9ff32586d4f
Antonio Guerriere <antonioguerriere@gmail.com>
2023-09-09 16:20:21 +0100

    added a hamburger menu for mobile

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleButton = document.querySelector(".toggle-button")
+const links = document.querySelector(".links")
+let toggled = false;
+
+toggleButton.addEventListener('click', () => {
+  toggled = !toggled; // Toggle the value of 'toggled' (true -> false, false -> true)
+  links.style.display = toggled ? "flex" : "none"; // Use the ternary operator to set the display property
+});
