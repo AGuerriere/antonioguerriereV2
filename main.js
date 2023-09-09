23f895cf5c0fbe5d87388c494e2b79ecfbe3909a
Antonio Guerriere <antonioguerriere@gmail.com>
2023-09-09 16:34:45 +0100

    change position of toggle button when the menu is open

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ let toggled = false;
 toggleButton.addEventListener('click', () => {
   toggled = !toggled; // Toggle the value of 'toggled' (true -> false, false -> true)
   links.style.display = toggled ? "flex" : "none"; // Use the ternary operator to set the display property
+  toggleButton.style.top = toggled ? "0.75rem" : "auto";
 });
